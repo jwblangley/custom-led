7063b74454fed297a1828eb88a5ee7be1b6f3390
James Langley <jwblangley@gmail.com>
2025-12-23 20:56:48 +0000

    Set some virtual LEDs

--- test_server.py
+++ test_server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import sys
 import logging
+import argparse
 
 from pprint import pformat
 
@@ -21,21 +22,34 @@ logging.basicConfig(
 
 
 class LEDManager:
-    def __init__(self):
-        pass
+    def __init__(self, num_leds):
+        self.leds = [(0, 0, 0)] * num_leds
 
     def set(self, set_leds: led_pb2.SetLEDs):
         logging.info("Setting LEDs")
-        leds = [(p.red, p.green, p.blue) for p in set_leds.pixels]
-        logging.debug(pformat(leds))
+
+        if len(set_leds.pixels) > len(self.leds):
+            logging.error(
+                f"Request to set more LEDs than configured. request={len(leds)}, configured={len(self.leds)}"
+            )
+            return
+
+        self.leds = [(p.red, p.green, p.blue) for p in set_leds.pixels]
+        logging.debug(pformat(self.leds))
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Test LED server")
+    parser.add_argument(
+        "-n", "--num-leds", help="Number of LEDs configured", required=True, type=int
+    )
+    args = parser.parse_args()
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((IP, PORT))
     logging.info(f"Listening on {IP}:{PORT}")
 
-    led_manager = LEDManager()
+    led_manager = LEDManager(args.num_leds)
 
     try:
         while True:
